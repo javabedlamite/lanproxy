4ee8ac5f82af1947e227d5f953031baf517099d8
javabedlamite <java_bedlamite@yeah.net>
2017-11-12 20:25:06 +0800

    12

--- proxy-server/src/main/java/org/fengfei/lanproxy/server/config/ProxyConfig.java
+++ proxy-server/src/main/java/org/fengfei/lanproxy/server/config/ProxyConfig.java
@@ -39,7 +39,7 @@ public class ProxyConfig implements Serializable {
     static {
 
         // 代理配置信息存放在用户根目录下
-        String dataPath =  "/app/" + ".lanproxy/";
+        String dataPath =  "/app/";
         File file = new File(dataPath);
         if (!file.isDirectory()) {
             file.mkdir();
